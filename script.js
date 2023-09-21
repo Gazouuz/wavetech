d35d048de2e21672cc35332177cb842a91bccc5e
Noan <delatouchenoan@gmail.com>
2023-09-21 22:30:07 +0200

    Made modifications and started to design the website

---
+++ script.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x5ff3f8,
+    color2: 0x5ff3f8,
+    backgroundColor: 0x161a2a,
+    spacing: 18.00,
+    points: 20.00,
+  })
